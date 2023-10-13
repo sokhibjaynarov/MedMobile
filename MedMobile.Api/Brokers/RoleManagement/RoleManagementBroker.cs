dbf4d64222cff2510c357ac24c72788e877bad06
Sokhib Jaynarov <s.jaynarov@baik.uz>
2023-10-14 04:30:25 +0500

    BROKERS: User and Role Manager and Log Brokers and SeedData Added

---
+++ MedMobile.Api/Brokers/RoleManagement/RoleManagementBroker.cs
@@ -0,0 +1,55 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) DevZilla team
+// ---------------------------------------------------------------
+
+using MedMobile.Api.Brokers.RoleManagement;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using MedMobile.Api.Models.Roles;
+
+namespace MedMobile.Api.Brokers.RoleManagement
+{
+    public class RoleManagementBroker : IRoleManagementBroker
+    {
+        private readonly RoleManager<Role> roleManagement;
+
+        public RoleManagementBroker(RoleManager<Role> roleManager)
+        {
+            this.roleManagement = roleManager;
+        }
+        public IQueryable<Role> SelectAllRoles() => this.roleManagement.Roles;
+
+        public async ValueTask<Role> SelectRoleByIdAsync(Guid RoleId)
+        {
+            var broker = new RoleManagementBroker(this.roleManagement);
+
+            return await broker.roleManagement.FindByIdAsync(RoleId.ToString());
+        }
+
+        public async ValueTask<Role> InsertRoleAsync(Role Role)
+        {
+            var broker = new RoleManagementBroker(this.roleManagement);
+            await broker.roleManagement.CreateAsync(Role);
+
+            return Role;
+        }
+
+        public async ValueTask<Role> UpdateRoleAsync(Role Role)
+        {
+            var broker = new RoleManagementBroker(this.roleManagement);
+            await broker.roleManagement.UpdateAsync(Role);
+
+            return Role;
+        }
+
+        public async ValueTask<Role> DeleteRoleAsync(Role Role)
+        {
+            var broker = new RoleManagementBroker(this.roleManagement);
+            await broker.roleManagement.DeleteAsync(Role);
+
+            return Role;
+        }
+    }
+}
---
+++ MedMobile.Api/Brokers/UserManagement/IUserManagementBroker.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) DevZilla team
+// ---------------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using MedMobile.Api.Models.Users;
+
+namespace MedMobile.Api.Brokers.UserManagement
+{
+    public interface IUserManagementBroker
+    {
+        ValueTask<User> InsertUserAsync(User user, string password);
+        IQueryable<User> SelectAllUsers();
+        ValueTask<User> SelectUserByIdAsync(Guid userId);
+        ValueTask<User> UpdateUserAsync(User user);
+        ValueTask<User> DeleteUserAsync(User user);
+    }
+}
