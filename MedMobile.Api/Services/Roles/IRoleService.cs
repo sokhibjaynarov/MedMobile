05acaf171a3679cd3c4e8e4d53ee490cb9d83349
Sokhib Jaynarov <s.jaynarov@baik.uz>
2023-10-14 04:49:18 +0500

    SERVICE: RoleService Added

--- MedMobile.Api/Services/Roles/IRoleService.cs
+++ MedMobile.Api/Services/Roles/IRoleService.cs
@@ -1,6 +1,18 @@
-﻿namespace MedMobile.Api.Services.Roles
+﻿// --------------------------------------------------------------- 
+// Copyright (c) DevZilla team
+// ---------------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using MedMobile.Api.Models.Roles;
+
+namespace MedMobile.Api.Services.Roles
 {
     public interface IRoleService
     {
+        ValueTask<Role> AddRoleAsync(string roleName);
+        ValueTask<Role> RetrieveRoleByIdAsync(Guid roleId);
+        IQueryable<Role> RetrieveAllRoles();
     }
 }
--- MedMobile.Api/Services/Roles/RoleService.cs
+++ MedMobile.Api/Services/Roles/RoleService.cs
@@ -1,6 +1,79 @@
-﻿namespace MedMobile.Api.Services.Roles
+﻿// --------------------------------------------------------------- 
+// Copyright (c) DevZilla team
+// ---------------------------------------------------------------
+
+using MedMobile.Api.Brokers.Loggings;
+using MedMobile.Api.Brokers.RoleManagement;
+using MedMobile.Api.Models.Roles;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedMobile.Api.Services.Roles
 {
-    public class RoleService
+    public class RoleService : IRoleService
     {
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IRoleManagementBroker roleManagementBroker;
+
+        public RoleService(ILoggingBroker loggingBroker, IRoleManagementBroker roleManagementBroker)
+        {
+            this.loggingBroker = loggingBroker;
+            this.roleManagementBroker = roleManagementBroker;
+        }
+
+        public async ValueTask<Role> AddRoleAsync(string roleName)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(roleName))
+                {
+                    var role = new Role();
+                    role.Name = roleName.Trim();
+
+                    return await this.roleManagementBroker.InsertRoleAsync(role);
+                }
+
+                throw new Exception();
+            }
+            catch (Exception ex)
+            {
+                loggingBroker.LogError(ex);
+                throw;
+            }
+        }
+
+        public IQueryable<Role> RetrieveAllRoles()
+        {
+            try
+            {
+                return this.roleManagementBroker.SelectAllRoles();
+            }
+            catch (Exception ex)
+            {
+                loggingBroker.LogError(ex);
+                throw;
+            }
+        }
+
+        public async ValueTask<Role> RetrieveRoleByIdAsync(Guid roleId)
+        {
+            try
+            {
+                Role role = await this.roleManagementBroker.SelectRoleByIdAsync(roleId);
+
+                if (role == null)
+                {
+                    throw new Exception();
+                }
+
+                return role;
+            }
+            catch (Exception ex)
+            {
+                loggingBroker.LogError(ex);
+                throw;
+            }
+        }
     }
 }
