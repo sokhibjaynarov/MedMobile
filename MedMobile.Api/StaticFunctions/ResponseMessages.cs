243954f25d64ff77cdbe129e553442d66a47c2f8
Nafisa <o.nafisa.b@gmail.com>
2023-10-14 12:23:41 +0500

    ResponseMessages added

---
+++ MedMobile.Api/StaticFunctions/ResponseMessages.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) DevZilla team
+// ---------------------------------------------------------------
+
+namespace MedMobile.Api.StaticFunctions
+{
+    public static class ResponseMessages
+    {
+        public const string ERROR_NOT_FOUND_DATA = "Error, not found data";
+        public const string ERROR_NOT_ALLOWED_DATA = "Error, not allowed data";
+        public const string ERROR_INVALID_DATA = "Error, invalid data";
+        public const string ERROR_EXIST_DATA = "Error, data already exists";
+    }
+}
