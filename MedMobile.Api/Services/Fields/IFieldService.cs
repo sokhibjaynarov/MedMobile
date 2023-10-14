7ce593fa88f95afce3d59365512603e99be706d0
Nafisa <o.nafisa.b@gmail.com>
2023-10-15 02:21:28 +0500

    GetAllFields method

--- MedMobile.Api/Services/Fields/IFieldService.cs
+++ MedMobile.Api/Services/Fields/IFieldService.cs
@@ -6,13 +6,14 @@ using MedMobile.Api.Models.Fields;
 using System.Linq;
 using System.Threading.Tasks;
 using System;
+using MedMobile.Api.ViewModels.Pagination;
 
 namespace MedMobile.Api.Services.Fields
 {
     public interface IFieldService
     {
         ValueTask<Field> AddFieldAsync(Field field);
-        IQueryable<Field> GetAllFields();
+        Task<PaginationResponse> GetAllFieldsAsync(string searchText, int skip, int take);
         ValueTask<Field> GetFieldByIdAsync(Guid fieldId);
         ValueTask<Field> ModifyFieldAsync(Field field);
         ValueTask<Field> RemoveFieldByIdAsync(Guid fieldId);
---
+++ MedMobile.Api/ViewModels/Fields/FieldForGetViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MedMobile.Api.ViewModels.Fields
+{
+    public class FieldForGetViewModel
+    {
+        public Guid FieldId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
