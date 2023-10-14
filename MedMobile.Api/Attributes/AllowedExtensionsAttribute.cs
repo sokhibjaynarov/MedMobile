31336ce37b71cec2ec7fe38be3d6ade2fac619ae
Sokhib Jaynarov <s.jaynarov@baik.uz>
2023-10-14 07:17:07 +0500

    Attributes added

---
+++ MedMobile.Api/Attributes/AllowedExtensionsAttribute.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) DevZilla team
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+
+namespace MedMobile.Api.Attributes
+{
+    public class AllowedExtensionsAttribute : ValidationAttribute
+    {
+        private readonly string[] _extensions;
+        public AllowedExtensionsAttribute(string[] extensions)
+        {
+            _extensions = extensions;
+        }
+
+        protected override ValidationResult IsValid(
+        object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                var extension = Path.GetExtension(file.FileName);
+                if (!_extensions.Contains(extension.ToLower()))
+                {
+                    return new ValidationResult($"Only files with a valid extensions ({string.Join(", ", _extensions)}) are allowed.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- MedMobile.Api/ViewModels/Users/RegisterPatientViewModel.cs
+++ MedMobile.Api/ViewModels/Users/RegisterPatientViewModel.cs
@@ -2,6 +2,8 @@
 // Copyright (c) DevZilla team
 // ---------------------------------------------------------------
 
+using System.ComponentModel.DataAnnotations;
+
 namespace MedMobile.Api.ViewModels.Users
 {
     public class RegisterPatientViewModel
@@ -11,6 +13,7 @@ namespace MedMobile.Api.ViewModels.Users
         public string FatherName { get; set; }
         public string PassportNumber { get; set; }
         public string Email { get; set; }
+        [RegularExpression(@"^998[389][012345789][0-9]{7}$", ErrorMessage = "Phone number is not valid")]
         public string PhoneNumber { get; set; }
         public string Description { get; set; }
         public string Password { get; set; }
