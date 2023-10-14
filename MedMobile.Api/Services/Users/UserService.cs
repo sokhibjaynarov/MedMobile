0772945fc6404f401e21fb2b1bc45427a4d347f5
Sokhib Jaynarov <s.jaynarov@baik.uz>
2023-10-14 06:20:22 +0500

    SERVICE: Patient register interface and viewModels added

--- MedMobile.Api/Services/Users/UserService.cs
+++ MedMobile.Api/Services/Users/UserService.cs
@@ -1,6 +1,18 @@
-﻿namespace MedMobile.Api.Services.Users
+﻿// --------------------------------------------------------------- 
+// Copyright (c) DevZilla team
+// ---------------------------------------------------------------
+
+using MedMobile.Api.ViewModels.Users;
+using System;
+using System.Threading.Tasks;
+
+namespace MedMobile.Api.Services.Users
 {
-    public class UserService
+    public class UserService : IUserService
     {
+        public ValueTask<Guid> RegisterPatientAsync(RegisterPatientViewModel registerPatientViewModel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ MedMobile.Api/ViewModels/Users/RegisterPatientViewModel.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) DevZilla team
+// ---------------------------------------------------------------
+
+namespace MedMobile.Api.ViewModels.Users
+{
+    public class RegisterPatientViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PassportId { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Description { get; set; }
+    }
+}
